4df0283560644bf6c181d4820305df727c7d619c
Saajan Varghese <157048612+gundumani376@users.noreply.github.com>
2025-05-29 16:03:33 +0530

    commit

--- Frontend/expense-tracker/src/App.jsx
+++ Frontend/expense-tracker/src/App.jsx
@@ -2,3 +2,6 @@ import React from 'react';
 const App= () => {
   return (
     <div>App</div>
+  )
+}
+export default App;
---
+++ Frontend/expense-tracker/src/pages/Dashboard/expense.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+const expense = () => {
+    return (
+        <div>expense</div>
+    )
+}
+export default expense
